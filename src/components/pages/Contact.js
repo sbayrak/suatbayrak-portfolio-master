d8c66e2b9865af9cfe50a0cb2036d301fcbdce58
sbayrak <suat.bayrak@bilgiedu.net>
2021-01-20 16:42:18 +0300

    Working on Netlify form submission

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,30 +10,37 @@ const Contact = () => {
   return (
     <Fragment>
       <Navbar></Navbar>
-      <div
-        className='contact'
-        onMouseMove={(e) => console.log(e.clientY - e.target.offsetTop)}
-      >
+      <div className='contact'>
         <div className='contact-wrapper'>
           <div className='title'>
             <span>Contact me</span>
           </div>
           <div className='contact-form'>
-            <form className='myform' action='#'>
+            <form
+              className='myform'
+              name='contact'
+              netlify
+              netlify-honeypot='bot-field'
+              hidden
+            >
+              <input type='hidden' name='form-name' value='contact' />
               <input
                 type='text'
                 placeholder='Your Name'
+                name='name'
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               ></input>
               <input
                 type='email'
                 placeholder='Your E-Mail'
+                name='email'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               ></input>
               <textarea
                 placeholder='Your message'
+                name='message'
                 value={text}
                 onChange={(e) => setText(e.target.value)}
               ></textarea>
