d3fded00fee956ad59b99d30436257d7f9ac9655
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-01-24 16:33:19 +0300

    Index page responsiveness is done for all devices

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../../App.css';
 
 const Navbar = () => {
