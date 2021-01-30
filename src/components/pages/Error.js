3ed31392bfcce094ea59d1cd977b616eaaeec979
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-01-30 17:46:18 +0300

    Added 404 page and svg

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import NotFound from '../layout/404.svg';
 
 const Error = () => {
+  const styles = {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/');
+    }, 3000);
+  });
+
   return (
-    <div>
-      <h1>404</h1>
+    <div className='notfound' style={styles}>
+      <img src={NotFound} alt='404' rel='noreferrer'></img>
     </div>
   );
 };
