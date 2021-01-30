f7bb3fbc4f9de37f3f1eda880536e978e9db3823
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-01-30 18:02:14 +0300

    Adding some styled-components

--- src/components/pages/Inter1.js
+++ src/components/pages/Inter1.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Inter1 = () => {
+  const Link = styled.a`
+    color: rgb(190, 190, 190);
+    transition: ease-in-out 0.5s;
+    font-size: 50px;
+  `;
+
   return (
     <div className='inter1'>
       <div className='inter1-wrapper'>
         <span>Go into my GitHub Repository</span>
-        <a
+        <Link
           href='https://www.github.com/sbayrak'
           target='_blank'
           rel='noreferrer'
         >
           <i className='fab fa-github'></i>
-        </a>
+        </Link>
       </div>
     </div>
   );
--- src/components/pages/PortfolioItem.js
+++ src/components/pages/PortfolioItem.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React from 'react';
 import reactlogo from '../layout/react-logo.png';
 import mern from '../layout/mern.jpg';
 
-const PortfolioItem = ({repo}) => {
-    
-    return (
-        <div className='card'>
-        <div className='card-img'>
-          <img src={repo.img === 'mern'  ? mern : reactlogo  } alt='mern' />
-        </div>
-        <div className='card-title'>
-          <span>{repo.title}</span>
-        </div>
-        <div className='card-desc'>
-          <p>{repo.desc}</p>
-        </div>
-        <div className='card-links'>
-          <a
-            href={repo.link}
-            target='_blank'
-            rel='noreferrer'
-          >
-            Repository
-          </a>
-        </div>
+import styled from 'styled-components';
+const PortfolioItem = ({ repo }) => {
+  const Link = styled.a`
+    font-family: 'Poppins', sans-serif;
+    outline: none;
+    font-style: italic;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.3);
+    font-size: 20px;
+    border: none;
+    text-decoration: none;
+    padding: 7px 15px;
+    border-radius: 5px;
+    color: #fff;
+    background-color: #2b2b2b;
+    transition: ease-in-out 0.5s;
+  `;
+  return (
+    <div className='card'>
+      <div className='card-img'>
+        <img src={repo.img === 'mern' ? mern : reactlogo} alt='mern' />
       </div>
-    )
-}
+      <div className='card-title'>
+        <span>{repo.title}</span>
+      </div>
+      <div className='card-desc'>
+        <p>{repo.desc}</p>
+      </div>
+      <div className='card-links'>
+        <Link href={repo.link} target='_blank' rel='noreferrer'>
+          Repository
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default PortfolioItem
+export default PortfolioItem;
