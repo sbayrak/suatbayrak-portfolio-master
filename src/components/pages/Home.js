61a7aca29cc9e5688592e2bd7bbe34dda4b8c570
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-01-30 17:39:56 +0300

    Addressing some issues

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Typical from 'react-typical';
 
 const Home = () => {
+  const welcomer = {
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    height: '100vh',
+    width: '100%',
+  };
+  const welcomerWrapper = {
+    width: '60%',
+    margin: '0 auto',
+    position: 'relative',
+    top: '35%',
+    textAlign: 'left',
+    borderRight: '1px solid rgba(207,207,207,0.5)',
+  };
+
+  const button = {
+    marginTop: '20px',
+    padding: '10px 20px',
+    fontSize: '54px',
+    borderRadius: '5px',
+    outline: 'none',
+    transition: 'ease-in-out 0.5s',
+    border: '1px solid white',
+    cursor: 'pointer',
+  };
+
   return (
     <div className='welcomer' id='welcomer'>
       <div className='welcomer-wrapper'>
